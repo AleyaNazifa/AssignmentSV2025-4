ca2a87ed570e0e1b5dc59e6f873b2edbe0e959eb
aleya nazifa <132155116+AleyaNazifa@users.noreply.github.com>
2025-11-01 15:51:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+import streamlit as st
+
+# --- Page Configuration ---
+st.set_page_config(
+    page_title="HFMD Malaysia Visualization",
+    page_icon="🦠"
+)
+
+# --- Import Pages ---
+home = st.Page(
+    'home.py',
+    title='Homepage',
+    icon=":material/home:",
+    default=True
+)
+
+visualise = st.Page(
+    'hfmd_visualisation.py',
+    title='HFMD and Weather Analysis',
+    icon=":material/bar_chart:"
+)
+
+# --- Navigation Menu ---
+pg = st.navigation(
+    {
+        "Menu": [home, visualise]
+    }
+)
+
+pg.run()
 
